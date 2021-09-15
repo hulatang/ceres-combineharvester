b912f5908ca537285bf31c1590de8bc3c7873d10
hulatang <hulatang_eric@163.com>
2021-09-16 00:54:53 +0800

    feat(ceres all coins config, costants): add all coins config and all coins default constants

---
+++ ceres/util/ceres_all_coins_config.py
@@ -0,0 +1,27 @@
+from ceres.consensus.constants import ConsensusConstants
+import os
+import pathlib
+from ceres.util.config import get_all_coin_names, load_config_cli
+
+
+COIN_NAMES = get_all_coin_names()
+
+def get_all_coins_config(service_name: str, coin_names=COIN_NAMES):
+
+    all_coins_configs = {}
+
+
+    for coin in COIN_NAMES:
+            coin_root_path = pathlib.Path(os.path.expanduser(os.getenv(f"{coin.upper()}_ROOT", f"~/.{coin.lower()}/mainnet"))).resolve()
+            coin_config = load_config_cli(coin_root_path, "config.yaml", service_name)
+            # connect_peers[coin] = [PeerInfo(coin_config["farmer_peer"]["host"], coin_config["farmer_peer"]["port"])]
+            overrides = coin_config["network_overrides"]["constants"][coin_config["selected_network"]]
+            coin_updated_constants = ConsensusConstants.replace_str_to_bytes(**overrides)
+            all_coins_configs[coin] = {}
+            all_coins_configs[coin]["config"] = coin_config
+            all_coins_configs[coin]["constants"] = coin_updated_constants
+        
+    print('ok')
+
+get_all_coins_config('harvester')
+
