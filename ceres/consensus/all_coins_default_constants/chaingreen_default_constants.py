a75715b92dafca0001f0442782438df130976792
hulatang <hulatang_eric@163.com>
2021-09-16 03:25:29 +0800

    chore(all coins default constants): add all coin's default constants files

---
+++ ceres/consensus/all_coins_default_constants/chaingreen_default_constants.py
@@ -0,0 +1,62 @@
+from chaingreen.util.ints import uint64
+
+from .constants import ConsensusConstants
+
+testnet_kwargs = {
+    "SLOT_BLOCKS_TARGET": 32,
+    "MIN_BLOCKS_PER_CHALLENGE_BLOCK": 16,  # Must be less than half of SLOT_BLOCKS_TARGET
+    "MAX_SUB_SLOT_BLOCKS": 128,  # Must be less than half of SUB_EPOCH_BLOCKS
+    "NUM_SPS_SUB_SLOT": 64,  # Must be a power of 2
+    "SUB_SLOT_ITERS_STARTING": 2 ** 16,
+    # DIFFICULTY_STARTING is the starting difficulty for the first epoch, which is then further
+    # multiplied by another factor of DIFFICULTY_CONSTANT_FACTOR, to be used in the VDF iter calculation formula.
+    "DIFFICULTY_CONSTANT_FACTOR": 2 ** 20,
+    "DIFFICULTY_STARTING": 1,
+    "DIFFICULTY_CHANGE_MAX_FACTOR": 3,  # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
+    "DIFFICULTY_CHANGE_MAX_FACTOR_v1_2_0": 81,  # This difficulty change to be applied with v1.2.0
+    "v1_2_0_ACTIVATION_BLOCK": 170496,  # activation of v1.2.0 rules height
+    # These 3 constants must be changed at the same time
+    "SUB_EPOCH_BLOCKS": 384,  # The number of blocks per sub-epoch, mainnet 384
+    "EPOCH_BLOCKS": 4608,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_BLOCKS
+    "SIGNIFICANT_BITS": 8,  # The number of bits to look at in difficulty and min iters. The rest are zeroed
+    "DISCRIMINANT_SIZE_BITS": 1024,  # Max is 1024 (based on ClassGroupElement int size)
+    "NUMBER_ZERO_BITS_PLOT_FILTER": 9,  # H(plot signature of the challenge) must start with these many zeroes
+    "MIN_PLOT_SIZE": 32,  # 32 for mainnet
+    "MAX_PLOT_SIZE": 50,
+    "SUB_SLOT_TIME_TARGET": 600,  # The target number of seconds per slot, mainnet 600
+    "NUM_SP_INTERVALS_EXTRA": 3,  # The number of sp intervals to add to the signage point
+    "MAX_FUTURE_TIME": 5 * 60,  # The next block can have a timestamp of at most these many seconds in the future
+    "NUMBER_OF_TIMESTAMPS": 11,  # Than the average of the last NUMBER_OF_TIMESTAMPS blocks
+    # Used as the initial cc rc challenges, as well as first block back pointers, and first SES back pointer
+    # We override this value based on the chain being run (testnet0, testnet1, mainnet, etc)
+    # Default used for tests is std_hash(b'')
+    "GENESIS_CHALLENGE": bytes.fromhex("2cef93a2ebf7c0546609311684e484f08555d3c0e4336a30a044e1ba7f26f691"),
+    # Forks of chia should change this value to provide replay attack protection. This is set to mainnet genesis chall
+    "AGG_SIG_ME_ADDITIONAL_DATA": bytes.fromhex("0235e47be80dbba72e8e105f87776fe16690838dde7f71e8a77086c0374bcaf3"),
+    "GENESIS_PRE_FARM_POOL_PUZZLE_HASH": bytes.fromhex(
+        "42e52e6fac97b0f409c372865989bc9443755887ab24cab3b00bcd1fdacad7f5"
+    ),
+    "GENESIS_PRE_FARM_FARMER_PUZZLE_HASH": bytes.fromhex(
+        "427930199ca2f1d7669de76057fc215abed4cf28e33ffdf662873f6d6622e3e6"
+    ),
+    "MAX_VDF_WITNESS_SIZE": 64,
+    # Size of mempool = 50x the size of block
+    "MEMPOOL_BLOCK_BUFFER": 50,
+    # Max coin amount, fits into 64 bits
+    "MAX_COIN_AMOUNT": uint64((1 << 64) - 1),
+    # Max block cost in clvm cost units
+    "MAX_BLOCK_COST_CLVM": 11000000000,
+    # The cost per byte of generator program
+    "COST_PER_BYTE": 12000,
+    "WEIGHT_PROOF_THRESHOLD": 2,
+    "BLOCKS_CACHE_SIZE": 4608 + (128 * 4),
+    "WEIGHT_PROOF_RECENT_BLOCKS": 1000,
+    "MAX_BLOCK_COUNT_PER_REQUESTS": 32,  # Allow up to 32 blocks per request
+    "INITIAL_FREEZE_END_TIMESTAMP": 1621630800,  # 2021-05-21T21:00:00Z
+    "NETWORK_TYPE": 0,
+    "MAX_GENERATOR_SIZE": 1000000,
+    "MAX_GENERATOR_REF_LIST_SIZE": 512,  # Number of references allowed in the block generator ref list
+}
+
+
+DEFAULT_CONSTANTS = ConsensusConstants(**testnet_kwargs)  # type: ignore
