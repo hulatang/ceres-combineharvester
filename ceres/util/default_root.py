6b143eca5b53f8f2d86841e0c3a50e1e15be9869
hulatang <hulatang_eric@163.com>
2021-09-18 14:08:54 +0800

    refactor(default root): change default to ./ceres/mainnet

--- ceres/util/default_root.py
+++ ceres/util/default_root.py
@@ -2,8 +2,11 @@ import os
 from pathlib import Path
 
 DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/mainnet"))).resolve()
-
 DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
+
+
+DEFAULT_CERES_ROOT_PATH = Path(os.path.expanduser(os.getenv("CERES_ROOT", "~/.ceres/mainnet"))).resolve()
+DEFAULT_CERES_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CERES_KEYS_ROOT", "~/.ceres_keys"))).resolve()
 
 
 def get_coin_root_path(coin: str="chia"):
