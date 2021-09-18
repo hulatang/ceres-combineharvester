01a406944dab3811716a0b53c5b39922fa8fdd1c
hulatang <hulatang_eric@163.com>
2021-09-18 14:10:54 +0800

    fix(default root): remove default root of chia

--- ceres/util/default_root.py
+++ ceres/util/default_root.py
@@ -1,13 +1,15 @@
 import os
 from pathlib import Path
 
-DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/mainnet"))).resolve()
-DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
-
+# DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/mainnet"))).resolve()
+# DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
 
 DEFAULT_CERES_ROOT_PATH = Path(os.path.expanduser(os.getenv("CERES_ROOT", "~/.ceres/mainnet"))).resolve()
 DEFAULT_CERES_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CERES_KEYS_ROOT", "~/.ceres_keys"))).resolve()
 
+
+DEFAULT_ROOT_PATH = DEFAULT_CERES_ROOT_PATH 
+DEFAULT_KEYS_ROOT_PATH = DEFAULT_CERES_KEYS_ROOT_PATH
 
 def get_coin_root_path(coin: str="chia"):
     return Path(os.path.expanduser(os.getenv(f"{coin.upper()}_ROOT", f"~/.{coin.lower()}/mainnet"))).resolve()
