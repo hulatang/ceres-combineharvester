8f798f0b6612f831f851eae33e5f65a526439603
hulatang <hulatang_eric@163.com>
2021-09-22 21:21:09 +0800

    feat(update_handshake): update handshake info by new coin root path

--- ceres/util/update_handshake_args.py
+++ ceres/util/update_handshake_args.py
@@ -1,3 +1,5 @@
+from ceres.util.default_root import DEFAULT_CERES_ROOT_PATH
+from ceres.util.config import load_config
 from ceres.util.ceres_all_coins_config import get_all_coins_config_constants
 from ceres.protocols.shared_protocol import Handshake
 # from ceres.util.config import get_all_coin_config
@@ -9,8 +11,10 @@ from ceres.util.ints import uint16, uint8
 
 
 def update_handshake_args(coin: str, **handshake_args):
-    coin_config = get_all_coins_config_constants()[coin]
-    for arg, value in coin_config.items():
+    # coin_config = get_all_coins_config_constants()[coin]
+    network_ids = load_config(DEFAULT_CERES_ROOT_PATH, filename="coins_config.yaml", sub_config="network_ids")
+
+    for arg, value in network_ids[coin].items():
         handshake_args[arg] = value
     
 
